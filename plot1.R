a68fd2ba5a886f0542ac88df8f93b0e058bf4f79
spymark <smarketos@gmail.com>
2015-01-11 12:47:26 +0000

    commiting initial solution, some changes still to be made

---
+++ plot1.R
@@ -0,0 +1,57 @@
+library(data.table)
+
+setwd('D:\\Coursera\\Exploratory Data Analysis\\ExData_Plotting1')
+
+#define the range of minutes we are interested in loading
+dtime <- difftime(as.POSIXct("2007-02-03"), as.POSIXct("2007-02-01"),units="mins")
+#convert the aforementioned time difference to numeric
+rowsToRead <- as.numeric(dtime)
+#using the fread function from data.table package we read only the rows we need, starting from '1/2/2007
+#and proceeding for as many rows as we calculated that fall into the range of interest
+#at the same time converting ? to empty string
+df <- fread("household_power_consumption.txt", skip="1/2/2007", nrows = rowsToRead, na.strings = c("?", ""))
+#properly set the names of df, using the appropriate data table method
+setnames(df,names(df),c('Date', 'Time', 'Global_active_power', 'Global_reactive_power', 'Voltage', 'Global_intensity', 
+                        'Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+
+#introduce a new column with both date and time in the correct format
+df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S") 
+
+#plot1
+hist(df$Global_active_power, main = 'Global Active Power')
+
+#plot2
+#png(filename = 'plot2.png', width = 480, height = 480, units = "px")
+
+plot(df$DateTime, df$Global_active_power, type='n',xlab = 'LOL', ylab ='FATO')
+lines(x=df$DateTime, y = df$Global_active_power, type = 'l')
+
+#dev.off()
+
+#plot3
+plot(df$DateTime, df$Sub_metering_1, type='n',xlab = 'LOL', ylab ='FATO')
+lines(x=df$DateTime, y = df$Sub_metering_1, type = 'l', col = 'black')
+lines(x=df$DateTime, y = df$Sub_metering_2, type = 'l', col = 'red')
+lines(x=df$DateTime, y = df$Sub_metering_3, type = 'l', col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 
+'Sub_metering_2', 'Sub_metering_1'))
+
+
+#plot4
+par(mfrow= c(2,2))
+
+plot(df$DateTime, df$Global_active_power, type='n',xlab = 'LOL', ylab ='FATO')
+lines(x=df$DateTime, y = df$Global_active_power, type = 'l')
+
+plot(c)
+
+plot(df$DateTime, df$Sub_metering_1, type='n',xlab = 'LOL', ylab ='FATO')
+lines(x=df$DateTime, y = df$Sub_metering_1, type = 'l', col = 'black')
+lines(x=df$DateTime, y = df$Sub_metering_2, type = 'l', col = 'red')
+lines(x=df$DateTime, y = df$Sub_metering_3, type = 'l', col = 'blue')
+
+plot(c)
+
+par(mfrow=c(1,1))
+
+
